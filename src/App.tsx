9516f24eb07f0ca7dfc444ce4b1f3f9c5f2bf717
Thayane Almeida <thayane.almeida196@gmail.com>
2023-04-17 21:46:02 -0300

    Tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './paginas/home/Home';
 import Navbar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer'
 import './App.css';
-
+import Login from './paginas/login/Login'
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </>
-   
-  );
+    <Router>
+      <Navbar />
+      <div style={{minHeight: '100vh'}}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+   );
 }
 
 export default App
